2318326b3a0413a4f6862092edb3f0c97abe0aa5
Yarin Omesi <yarinomesi@gmail.com>
2023-01-20 12:06:50 +0200

    Initialize Commit

---
+++ TestsHelper.SourceGenerator/MockFillerSourceGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace TestsHelper.SourceGenerator;
+
+[Generator]
+public class MockFillerSourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+    }
+}
