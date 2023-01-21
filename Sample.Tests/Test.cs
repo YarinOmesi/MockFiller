16b7b919c44f34b2ce85b3c01af18e0bd0c62759
Yarin Omesi <yarinomesi@gmail.com>
2023-01-21 20:09:32 +0200

    Add Sample Code

---
+++ Sample.Tests/Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+using TestsHelper.SourceGenerator.Attributes;
+
+namespace Sample.Tests;
+
+[TestFixture]
+public partial class Test
+{
+    [FillMocks]
+    private TestedClass _testedClass;
+
+    
+    private readonly ILoggerFactory _defaultValueFactory = NullLoggerFactory.Instance;
+
+    [SetUp]
+    public void Setup()
+    {
+        _testedClass = Build();
+    }
+
+    [Test]
+    public void METHOD_On_Effect()
+    {
+        // Arrange
+        
+        // Act
+        _testedClass.VeryComplicatedLogic();
+        // Assert
+
+    }
+}
+
---
+++ Sample.Tests/TestedClass.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Sample.Tests;
+
+public class TestedClass
+{
+    private IDependency _dependency;
+    private ILogger _logger;
+
+    public TestedClass(IDependency dependency, ILoggerFactory factory)
+    {
+        _dependency = dependency;
+        _logger = factory.CreateLogger<TestedClass>();
+    }
+
+    public int VeryComplicatedLogic()
+    {
+        return 0;
+    }
+}
