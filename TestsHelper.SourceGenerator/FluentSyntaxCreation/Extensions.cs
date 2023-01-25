98955983d85055d81134fe8f9cf1231247ae41a5
Yarin Omesi <yarinomesi@gmail.com>
2023-01-25 21:14:56 +0200

    Add Syntax Extensions Methods

---
+++ TestsHelper.SourceGenerator/FluentSyntaxCreation/Extensions.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+
+namespace TestsHelper.SourceGenerator.FluentSyntaxCreation;
+
+public static class Extensions
+{
+    public static GenericNameSyntax Generic(this SyntaxToken type, params TypeSyntax[] arguments) =>
+        GenericName(type, TypeArgumentList(SeparatedList(arguments)));
+
+    public static GenericNameSyntax Generic(this SyntaxToken type, IEnumerable<TypeSyntax> arguments) =>
+        GenericName(type, TypeArgumentList(SeparatedList(arguments)));
+
+    public static GenericNameSyntax Generic(this string type, IEnumerable<TypeSyntax> arguments) => Identifier(type).Generic(arguments);
+
+    public static GenericNameSyntax Generic(this string type, params string[] arguments) =>
+        type.Generic(arguments.Select(IdentifierName));
+
+    public static GenericNameSyntax Generic(this string type, IEnumerable<string> arguments) =>
+        type.Generic(arguments.Select(IdentifierName));
+
+    public static GenericNameSyntax Generic(this string type, TypeSyntax argument) =>
+        GenericName(Identifier(type), TypeArgumentList(SingletonSeparatedList(argument)));
+
+    public static GenericNameSyntax Generic(this string type, string typedName) =>
+        GenericName(Identifier(type), TypeArgumentList(SingletonSeparatedList<TypeSyntax>(IdentifierName(typedName))));
+
+    public static MemberAccessExpressionSyntax AccessMember(this string instance, string member) =>
+        MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, IdentifierName(instance), IdentifierName(member));
+
+    public static MemberAccessExpressionSyntax AccessMember(this string instance, SimpleNameSyntax member) =>
+        MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, IdentifierName(instance), member);
+
+    public static MemberAccessExpressionSyntax AccessMember(this ExpressionSyntax instance, string member) =>
+        MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, instance, IdentifierName(member));
+
+    public static InvocationExpressionSyntax Invoke(this ExpressionSyntax instance) => InvocationExpression(instance);
+
+    public static InvocationExpressionSyntax Invoke(this ExpressionSyntax instance, IEnumerable<ExpressionSyntax> arguments)
+    {
+        return instance.Invoke(arguments.ToArray());
+    }
+
+    public static InvocationExpressionSyntax Invoke(this ExpressionSyntax instance, params ExpressionSyntax[] arguments)
+    {
+        return InvocationExpression(instance, ArgumentList(SeparatedList(arguments.Select(Argument))));
+    }
+
+    public static InvocationExpressionSyntax Invoke(this ExpressionSyntax instance, ExpressionSyntax argument)
+    {
+        return InvocationExpression(instance, ArgumentList(SingletonSeparatedList(Argument(argument))));
+    }
+
+    public static BinaryExpressionSyntax Coalesce(this ExpressionSyntax left, ExpressionSyntax right)
+    {
+        // left ?? right
+        return BinaryExpression(SyntaxKind.CoalesceExpression, left, right);
+    }
+
+    public static IdentifierNameSyntax Name(this SyntaxToken node) => IdentifierName(node);
+
+    public static InitializerExpressionSyntax ArrayInitializer(this IEnumerable<ExpressionSyntax> values)
+    {
+        return InitializerExpression(SyntaxKind.ArrayInitializerExpression, SeparatedList(values));
+    }
+
+    public static ImplicitArrayCreationExpressionSyntax ImplicitCreation(this InitializerExpressionSyntax initializerExpressionSyntax)
+    {
+        return ImplicitArrayCreationExpression(initializerExpressionSyntax);
+    }
+
+    public static VariableDeclarationSyntax DeclareVariables(this TypeSyntax type, IEnumerable<VariableDeclaratorSyntax> variables)
+    {
+        return VariableDeclaration(type, SeparatedList(variables));
+    }
+
+    public static VariableDeclarationSyntax DeclareVariable(this TypeSyntax type, VariableDeclaratorSyntax variable)
+    {
+        return VariableDeclaration(type, SingletonSeparatedList(variable));
+    }
+
+    public static AssignmentExpressionSyntax Assign(this ExpressionSyntax container, ExpressionSyntax value)
+    {
+        return AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, container, value);
+    }
+
+    public static ExpressionStatementSyntax ToStatement(this ExpressionSyntax expressionSyntax) => ExpressionStatement(expressionSyntax);
+
+    public static VariableDeclaratorSyntax WithInitializer(this VariableDeclaratorSyntax var, ExpressionSyntax value)
+    {
+        return var.WithInitializer(EqualsValueClause(value));
+    }
+}
