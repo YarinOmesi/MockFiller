d7f6d13cd7436d768a8b113cf1ef02fded979a08
Yarin Omesi <yarinomesi@gmail.com>
2023-01-25 23:42:21 +0200

    Move Cyber Logics Into A Project And Created An Attribute For GenerateMockWrappers

---
+++ TestsHelper.SourceGenerator.MockWrapping/Value.cs
@@ -0,0 +1,21 @@
+﻿namespace TestsHelper.SourceGenerator.MockWrapping;
+
+public readonly struct Value<T>
+{
+    public static readonly Value<T> Any = new(default, isAny: true);
+    public static readonly Value<T> Default = new(default, isDefault: true);
+    
+    public T _Value { get; }
+    public bool IsAny { get; }
+    public bool IsDefault { get; }
+
+
+    public Value(T value, bool isDefault = false, bool isAny = false)
+    {
+        _Value = value;
+        IsDefault = isDefault;
+        IsAny = isAny;
+    }
+
+    public static implicit operator Value<T>(T value) => new(value);
+}
