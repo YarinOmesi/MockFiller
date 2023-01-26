a7451099a41f11ffbd6a3dcb5e7fbc13bf87cf3d
Yarin Omesi <yarinomesi@gmail.com>
2023-01-26 19:02:58 +0200

    Use Syntax Extension In `MockGenerator`

--- TestsHelper.SourceGenerator/MockFilling/PartialImplementation/Logics/MockGenerator.cs
+++ TestsHelper.SourceGenerator/MockFilling/PartialImplementation/Logics/MockGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TestsHelper.SourceGenerator.FluentSyntaxCreation;
 using TestsHelper.SourceGenerator.MockFilling.PartialImplementation.Models;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
@@ -14,13 +15,9 @@ public class MockGenerator
 
     private FieldDeclarationSyntax CreateMockField(Mock mock)
     {
-        GenericNameSyntax type = GenericName(
-            Identifier("Mock"),
-            TypeArgumentList(SingletonSeparatedList<TypeSyntax>(IdentifierName(mock.Type.Name)))
-        );
-
-        VariableDeclarationSyntax declaration = VariableDeclaration(type)
-            .AddVariables(VariableDeclarator($"_{mock.ParameterName}Mock"));
+        GenericNameSyntax type = "Mock".Generic(mock.Type.Name);
+        
+        VariableDeclarationSyntax declaration = type.DeclareVariable(VariableDeclarator($"_{mock.ParameterName}Mock"));
 
         return FieldDeclaration(declaration)
             .WithModifiers(TokenList(Token(SyntaxKind.PrivateKeyword)))
