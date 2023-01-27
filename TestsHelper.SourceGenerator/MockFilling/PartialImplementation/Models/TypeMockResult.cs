942cb9ba181d9bef2d93e5df7ffc6887e21ec5b2
Yarin Omesi <yarinomesi@gmail.com>
2023-01-28 01:13:57 +0200

    Add `TypeMockResult` as the result for class type wrapper

---
+++ TestsHelper.SourceGenerator/MockFilling/PartialImplementation/Models/TypeMockResult.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TestsHelper.SourceGenerator.MockFilling.PartialImplementation.Models;
+
+public readonly record struct TypeMockResult(
+    string Name, 
+    string Namespace,
+    GeneratedMock GeneratedMock,
+    PropertyDeclarationSyntax MockProperty,
+    CompilationUnitSyntax CompilationUnitSyntax)
+{
+    public string Name { get; } = Name;
+    public string Namespace { get; } = Namespace;
+    public PropertyDeclarationSyntax MockProperty { get; } = MockProperty;
+    public string MockPropertyName { get; } = MockProperty.Identifier.Text;
+    public CompilationUnitSyntax CompilationUnitSyntax { get; } = CompilationUnitSyntax;
+    public GeneratedMock GeneratedMock { get; } = GeneratedMock;
+
+    public ITypeSymbol WrappedType => GeneratedMock.Mock.Type;
+
+    public string ParameterName => GeneratedMock.ParameterName;
+}
