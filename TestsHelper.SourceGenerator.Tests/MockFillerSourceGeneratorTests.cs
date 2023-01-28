1226c8d1b5224d7ece4294f0b232f3e4ca36b798
Yarin Omesi <yarinomesi@gmail.com>
2023-01-28 15:08:46 +0200

    Removed Unused usings

--- TestsHelper.SourceGenerator.Tests/MockFillerSourceGeneratorTests.cs
+++ TestsHelper.SourceGenerator.Tests/MockFillerSourceGeneratorTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.IO;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp;
