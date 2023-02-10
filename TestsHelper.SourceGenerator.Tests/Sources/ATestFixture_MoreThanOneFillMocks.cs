5976125445d13de8d6d4b84f2c99369aaf076f0e
Yarin Omesi <yarinomesi@gmail.com>
2023-02-10 13:56:55 +0200

    Modify Files For Test To Use DefaultValue Attribute

--- TestsHelper.SourceGenerator.Tests/Sources/ATestFixture_MoreThanOneFillMocks.cs
+++ TestsHelper.SourceGenerator.Tests/Sources/ATestFixture_MoreThanOneFillMocks.cs
@@ -12,5 +12,7 @@ public partial class ATestFixture
     
     [FillMocks]
     private TestedClass _testedClass2;
-    private readonly ILoggerFactory _defaultValueFactory = NullLoggerFactory.Instance;
+
+    [DefaultValue("factory")]
+    private readonly ILoggerFactory _nullFactory = NullLoggerFactory.Instance;
 }
