1db2827ab90bd935b40fb4af0838dab456bffc23
Yarin Omesi <yarinomesi@gmail.com>
2023-02-10 14:45:20 +0200

    Add Test To Default Value Diagnostics

---
+++ TestsHelper.SourceGenerator.Tests/Sources/ATestFixture__DefaultValue_NonExistingParameter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+using TestsHelper.SourceGenerator.Attributes;
+
+namespace MyNamespace;
+
+public partial class ATestFixture
+{
+    [FillMocks]
+    private TestedClass _testedClass;
+    
+    [DefaultValue("NonExistingParameterName")]
+    private readonly ILoggerFactory _nullFactory = NullLoggerFactory.Instance;
+}
