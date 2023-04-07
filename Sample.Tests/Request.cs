ca790a2124dce3f24e788513f896a69534f357ab
Yarin Omesi <yarinomesi@gmail.com>
2023-04-07 19:31:49 +0300

    Ignore nullable warning

--- Sample.Tests/Request.cs
+++ Sample.Tests/Request.cs
@@ -2,6 +2,6 @@ namespace Sample.Tests;
 
 public class Request
 {
-    public string Url { get; init; } 
-    public string Body { get; init; }
+    public string Url { get; init; } = null!;
+    public string Body { get; init; } = null!;
 }
--- Sample.Tests/Response.cs
+++ Sample.Tests/Response.cs
@@ -2,6 +2,6 @@ namespace Sample.Tests;
 
 public class Response
 {
-    public string Url { get; init; } 
-    public string Body { get; init; }
+    public string Url { get; init; } = null!;
+    public string Body { get; init; } = null!;
 }
