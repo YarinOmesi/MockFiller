f0df5106e822f1b432716d13547dfbeccd00030d
Yarin Omesi <yarinomesi@gmail.com>
2023-04-12 02:59:55 +0300

    Create MoqValueConverter and interface

---
+++ TestsHelper.SourceGenerator.MockWrapping/Converters/MoqValueConverter.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Moq;
+using TestsHelper.SourceGenerator.MockWrapping.Values;
+
+namespace TestsHelper.SourceGenerator.MockWrapping.Converters;
+
+public class MoqValueConverter : IValueConverter
+{
+    public static readonly MoqValueConverter Instance = new(new Dictionary<Type, IValueConverter> {
+        [typeof(AnyValue<>)] = new AnyValueConverter(),
+        [typeof(ExactValue<>)] = new ExactValueConverter(),
+        [typeof(PredicateValue<>)] = new PredicateValueConverter()
+    });
+
+    private readonly Dictionary<Type, IValueConverter> _valueConverters;
+
+    private MoqValueConverter(Dictionary<Type, IValueConverter> valueConverters)
+    {
+        _valueConverters = valueConverters;
+    }
+
+    public Expression Convert<T>(Value<T> value)
+    {
+        value ??= Value<T>.Any;
+        
+        Type typeWithoutGenericArgument = value.GetType().GetGenericTypeDefinition();
+        if (_valueConverters.TryGetValue(typeWithoutGenericArgument, out IValueConverter converter))
+        {
+            return converter.Convert(value);
+        }
+
+        throw new ArgumentException($"Could not convert value of type {value.GetType()}");
+    }
+
+    private class AnyValueConverter : IValueConverter
+    {
+        public Expression Convert<T>(Value<T> value)
+        {
+            Expression<Func<T>> isAnyExpression = () => It.IsAny<T>();
+            return isAnyExpression.Body;
+        }
+    }
+    
+    private class ExactValueConverter : IValueConverter
+    {
+        public Expression Convert<T>(Value<T> value)
+        {
+            ExactValue<T> exactValue = (ExactValue<T>) value;
+            Expression<Func<T>> itIsExpression = () => It.Is<T>(Cyber.FillValue<T>(), EqualityComparer<T>.Default);
+            
+            return ExpressionUtils.UpdateFirstArgument(itIsExpression, Expression.Constant(exactValue.Value));
+        }
+    }
+    
+    private class PredicateValueConverter : IValueConverter
+    {
+        public Expression Convert<T>(Value<T> value)
+        {
+            PredicateValue<T> predicateValue = (PredicateValue<T>) value;
+            Expression<Func<T>> itIsExpression = () => It.Is<T>(Cyber.FillPredicate<T>());
+            
+            return ExpressionUtils.UpdateFirstArgument(itIsExpression, predicateValue.Predicate);
+        }
+    }
+}
