ab9ca8b0fb67a4552b9775837f4f674f85523329
Yarin Omesi <yarinomesi@gmail.com>
2023-08-05 01:26:25 +0300

    Create `AliasType` and `NullableAliasType`

---
+++ TestsHelper.SourceGenerator/CodeBuilding/Types/AliasType.cs
@@ -0,0 +1,16 @@
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TestsHelper.SourceGenerator.CodeBuilding.Types;
+
+public record AliasType(string Name, QualifiedNameSyntax AliasTo) : IType
+{
+    public string Namespace => string.Empty;
+
+    public virtual TypeSyntax Build() => SyntaxFactory.IdentifierName(Name);
+}
+
+public sealed record NullableAliasType(AliasType AliasType) : AliasType(AliasType)
+{
+    public override TypeSyntax Build() => SyntaxFactory.NullableType(AliasType.Build());
+}
